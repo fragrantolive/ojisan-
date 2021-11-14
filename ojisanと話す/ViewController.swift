300de96e176158a10f39f3d55357ee47fb96e565
nakajimashigusa <fragrantolive2005@gmail.com>
2021-11-14 11:42:11 +0900

    test

--- ojisanと話す/ViewController.swift
+++ ojisanと話す/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
         
         startButton.layer.cornerRadius = 125
         
